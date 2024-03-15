64faa7091aa92ea485f113fe431ff2a52f4b597d
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-03-15 18:37:55 +0530

    Initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,11 @@
+const Latitude=document.getElementById('Latitude');
+const Longitude=document.getElementById('Longitude');
+const Housing_median_age=document.getElementById('Housing-median-age');
+const Total_rooms=document.getElementById('Total-rooms');
+const Total_bedrooms=document.getElementById('Total-bedrooms');
+const Population=document.getElementById('Population');
+const Households=document.getElementById('Households');
+const Median_income=document.getElementById('Median-income');
+const Ocean_proximity=document.getElementById('Ocean-proximity');
+const button=document.querySelector('.btn');
+const predict=document.querySelector('.predict');
