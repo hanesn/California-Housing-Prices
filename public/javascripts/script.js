6b5448c287cff051e3f10206775a98031f0c883c
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-03-17 19:54:02 +0530

    Create submit function

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,3 +9,28 @@ const Median_income=document.getElementById('Median-income');
 const Ocean_proximity=document.getElementById('Ocean-proximity');
 const button=document.querySelector('.btn');
 const predict=document.querySelector('.predict');
+
+async function submit(){
+    try{
+        const response=await fetch('/submit',{
+            method:'post',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                Longitude:Longitude.value,
+                Latitude:Latitude.value,
+                Housing_median_age:Housing_median_age.value,
+                Total_rooms:Total_rooms.value,
+                Total_bedrooms:Total_bedrooms.value,
+                Population:Population.value,
+                Households:Households.value,
+                Median_income:Median_income.value,
+                Ocean_proximity:Ocean_proximity.value
+            })
+        });
+    }
+    catch(error){
+        console.log('Error: ',error);
+    }
+}
